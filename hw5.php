fe2ffe637386529ea6b8a060b3ae0a0fd65e299f
Olga <fedosova.olya@gmail.com>
2018-06-02 15:46:51 +0300

    lesson5

---
+++ hw5.php
@@ -0,0 +1,37 @@
+<?php
+
+$file = file_get_contents(__DIR__. '/phonebook.json');
+$data = json_decode($file, true);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>ДЗ5</title>
+</head>
+<body>
+
+  <table>
+    <tr>
+      <td>№</td>
+      <td>Имя</td>
+      <td>Фамилия</td>
+      <td>Адрес</td>
+      <td>Телефон</td>
+    </tr>
+    <?php foreach ($data as $key => $phonebook) {?>
+      <tr>
+        <td><?php echo $phonebook['id']; ?></td>
+        <td><?php echo $phonebook['firstname']; ?></td>
+        <td><?php echo $phonebook['lastname']; ?></td>
+        <td><?php echo $phonebook['address']; ?></td>
+        <td><?php echo $phonebook['phonenumber']; ?></td>
+      </tr>  
+    <?php } ?>
+  </table>
+
+</body>
+</html>
